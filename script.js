56207b1212bd7cf054baa84a2eb9be726dcb2a63
tiagomagalhaes242 <tiagomagalhaes242@gmail.com>
2026-09-20 15:04:55 -0300

    Conectar botoes ao ESP8266

--- script.js
+++ script.js
@@ -1,67 +1,13 @@
-﻿const message = document.getElementById("message");
-const status = document.getElementById("status");
+﻿const ESP_URL = "http://192.168.18.50";
+
+const message = document.getElementById("message");
 
 document.getElementById("lightOn").addEventListener("click", () => {
-  message.textContent = "Comando: ligar luz.";
-  console.log("LIGAR LUZ");
+    message.textContent = "Enviando comando para o ESP8266...";
+    window.open(`${ESP_URL}/ligar`, "_blank");
 });
 
 document.getElementById("lightOff").addEventListener("click", () => {
-  message.textContent = "Comando: desligar luz.";
-  console.log("DESLIGAR LUZ");
+    message.textContent = "Enviando comando para o ESP8266...";
+    window.open(`${ESP_URL}/desligar`, "_blank");
 });
-
-const SpeechRecognition =
-  window.SpeechRecognition ||
-  window.webkitSpeechRecognition;
-
-if (SpeechRecognition) {
-
-  const recognition = new SpeechRecognition();
-
-  recognition.lang = "pt-BR";
-  recognition.continuous = false;
-  recognition.interimResults = false;
-
-  document.getElementById("mic").addEventListener("click", () => {
-    message.textContent = "Estou ouvindo...";
-    recognition.start();
-  });
-
-  recognition.onresult = (event) => {
-
-    const text =
-      event.results[0][0].transcript.toLowerCase();
-
-    message.textContent = `Você disse: "${text}"`;
-
-    if (
-      text.includes("liga") &&
-      text.includes("luz")
-    ) {
-      document.getElementById("lightOn").click();
-    }
-
-    else if (
-      text.includes("desliga") &&
-      text.includes("luz")
-    ) {
-      document.getElementById("lightOff").click();
-    }
-
-  };
-
-  recognition.onerror = () => {
-    message.textContent =
-      "Não consegui entender o comando.";
-  };
-
-}
-else {
-
-  document.getElementById("mic").disabled = true;
-
-  message.textContent =
-    "Reconhecimento de voz não disponível neste navegador.";
-
-}
